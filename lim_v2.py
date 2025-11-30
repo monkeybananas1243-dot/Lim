ce6bab402c944a526b4d59974cf8b8cab3e1458c
zowlt <monkeybananas1243@gmail.com>
2025-11-30 15:58:20 -0800

    Adding a venv again. Using an older version of python.

--- lim_v2.py
+++ lim_v2.py
@@ -3,16 +3,21 @@ from PyQt5 import QtGui
 from PyQt5.QtGui import QFont
 import sys, os
 
-import tensorflow as tf # type: ignore
+import tensorflow as tf
 from datasets import load_dataset
 
-train_ds = tf.keras.utils.text_dataset_from_directory(
+dataset = load_dataset("squad")
+
+train_data = dataset['train']
+validation_data = dataset['validation']
+
+"""train_ds = tf.keras.utils.text_dataset_from_directory(
     'path/to/train_directory',
     batch_size=32,
     validation_split=0.2,
     subset='training',
     seed=123
-)
+)"""
 
 class MainWindow(QMainWindow):
     def __init__(self):
